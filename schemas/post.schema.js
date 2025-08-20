da88b83772fc0695cc9f48f52e6e6b6176aa2b79
deb-alien <DevPro-2011@outlook.com>
2025-08-20 23:21:24 +0600

    post delete feature, get signed urls helper function

--- schemas/post.schema.js
+++ schemas/post.schema.js
@@ -1,12 +1,8 @@
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import mongoose from 'mongoose';
-import { GetObjectCommand } from '@aws-sdk/client-s3';
-
-import { bucketName, s3 } from '../lib/s3/s3.js';
 
 const { Schema } = mongoose;
 
-const postSchema = new Schema(
+const PostSchema = new Schema(
 	{
 		title: {
 			type: String,
@@ -26,22 +22,6 @@ const postSchema = new Schema(
 	},
 );
 
-postSchema.virtual('imageUrl', async function () {
-	if(!this.image) return null
-
-	return await getSignedUrl(
-		s3,
-		new GetObjectCommand({
-			Bucket: bucketName,
-			Key: this.image
-		}),
-		{ expiresIn: 60 * 60 * 24 }
-	)
-})
-
-PostSchema.set('toObject', { virtuals: true });
-PostSchema.set('toJSON', { virtuals: true });
-
-const PostModel = mongoose.model('Post', postSchema);
+const PostModel = mongoose.model('Post', PostSchema);
 
 export default PostModel;
